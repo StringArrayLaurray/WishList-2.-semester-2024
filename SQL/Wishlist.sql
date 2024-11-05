3b8223f0f4801b7765ff3a94fb7473927fe37463
Laura Swienty Andresen <164322979+StringArrayLaurray@users.noreply.github.com>
2024-11-05 21:58:21 +0100

    SQL  kode tilføjet

---
+++ SQL/Wishlist.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS wishlist;
+CREATE SCHEMA IF NOT EXISTS wishlist;
+USE wishlist;
+
+CREATE TABLE user (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE wishlist (
+	wishlist_id INT AUTO_INCREMENT PRIMARY KEY,
+    wishlist_name VARCHAR(100),
+    user_id  INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE wish (
+	wish_id INT AUTO_INCREMENT PRIMARY KEY,
+    wish_name VARCHAR(100) NOT NULL,
+    wish_description TEXT,
+    wish_price DECIMAL(10, 2),
+    is_reserved BOOLEAN DEFAULT FALSE,
+    wish_link VARCHAR(255),
+    wishlist_id INT NOT NULL,
+    FOREIGN KEY (wishlist_id) REFERENCES wishlist(wishlist_id)
+);
+
