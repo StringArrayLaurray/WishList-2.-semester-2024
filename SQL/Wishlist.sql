256006ff2c6daac34888a6a85a0c585b79c21802
Celine Lund Månsson <166127204+Celinelundm@users.noreply.github.com>
2024-11-07 23:35:16 +0100

    Update Wishlist.sql
    Ingen is_reserved

--- SQL/Wishlist.sql
+++ SQL/Wishlist.sql
@@ -6,7 +6,7 @@ CREATE TABLE user (
 	user_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUEuser,
     password VARCHAR(255) NOT NULL,
     username VARCHAR(100) NOT NULL
 );
@@ -23,9 +23,7 @@ CREATE TABLE wish (
     wish_name VARCHAR(100) NOT NULL,
     wish_description TEXT,
     wish_price DECIMAL(10, 2),
-    is_reserved BOOLEAN DEFAULT FALSE,
     wish_link VARCHAR(255),
     wishlist_id INT NOT NULL,
     FOREIGN KEY (wishlist_id) REFERENCES wishlist(wishlist_id)
 );
-
