a43ac5f78ca8e7918e451dfe7a147643d35f9e34
StringArrayLaurray <laan0004@stud.kea.dk>
2024-11-11 20:42:27 +0100

    ændret CSS

--- src/main/java/org/example/wishlist2semester2024/repository/WishRepository.java
+++ src/main/java/org/example/wishlist2semester2024/repository/WishRepository.java
@@ -5,7 +5,6 @@ import org.example.wishlist2semester2024.model.Wish;
 import org.example.wishlist2semester2024.model.Wishlist;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.tree.RowMapper;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
